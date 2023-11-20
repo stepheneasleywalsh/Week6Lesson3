97933ce5a5e9fb47e0709b7c756bf23c517deadc
Stephen Easley-Walsh <stephen.easley-walsh@studygroup.com>
2023-11-20 21:08:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def factorial(x):
-    if x <= 0:
+    if x < 0:
         return("Error")
-    elif x == 1:
+    elif x == 1 or x == 0:
         return 1
     else:
         return x*factorial(x-1)
 
-x = int(input("Input an x and I will calculate x! "))
+x = int(input("Input an x and I will calculate x factorial. "))
 
 print(factorial(x))
